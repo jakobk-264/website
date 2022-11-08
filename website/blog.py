0b0b007c0c7f8a88af74dd3bec23e98b10acaf50
Jakob K <75134220+jakobk-264@users.noreply.github.com>
2022-11-08 20:21:31 +0000

    unit testing

--- website/blog.py
+++ website/blog.py
@@ -46,8 +46,8 @@ def create():
                 " VALUES (?, ?, ?)",
                 (title, body, g.user["id"]),
             )
-        db.commit()
-        return redirect(url_for("blog.index"))
+            db.commit()
+            return redirect(url_for("blog.index"))
 
     return render_template("blog/create.html")
 
